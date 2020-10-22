e2b29fddb9e15f2dbe65e150be8e226aa57fdebf
Martin Degiorgio <martin.degiorgio@go.com.mt>
2020-10-22 12:19:02 +0200

    Updated spring-boot-starter-parent version Added new requestMapping to test different annotations

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -4,9 +4,7 @@ import com.example.accessingdatamongodb.model.Customer;
 import com.example.accessingdatamongodb.repository.CustomerRepository;
 import com.example.accessingdatamongodb.service.CustomerService;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +24,32 @@ public class ApplicationController implements CommandLineRunner {
 		this.customerService = customerService;
 	}
 
-	@GetMapping("/customer")
-	public List<String> getDetailsByName(@RequestParam(value = "name", defaultValue = ALL_VALUES) String name) {
+	//This method has been done to test different options in requestMapping annotation
+	//examples can be as follows:
+	//http://localhost:8080/customerName/msisdn:35679632198?fields=DOB
+	//http://localhost:8080/customerName/msisdn:35679632198
+	@RequestMapping(value = "/customerName/{identifierType}:{serviceId}")
+	@ResponseBody
+	public String getCustomer(
+			@PathVariable String identifierType,
+			@PathVariable String serviceId,
+			@RequestParam(value = "fields", required = false) String params)
+	{
+		return "The selected parameters are identifierType / serviceId / params : >>"
+				+ identifierType + "<< >>" + serviceId + "<< >>>" + params ;
+
+	}
+
+	//@GetMapping("/customer")
+	@RequestMapping(value = "/customer", method = RequestMethod.GET)
+	public List<String> getDetailsByName(
+			@RequestParam(value = "name", defaultValue = ALL_VALUES)
+			String name)
+	{
+
 		List<String> customers = new ArrayList<>();
 		if (name.equals(ALL_VALUES) || name.equals(ALL_VALUES.toLowerCase())) {
-			 return customerService.findAll().stream().map(Customer::toString).collect(Collectors.toList());
+			return customerService.findAll().stream().map(Customer::toString).collect(Collectors.toList());
 		} else {
 			Customer customerRec = customerService.findByFirstName(name);
 			if (customerRec != null) {
