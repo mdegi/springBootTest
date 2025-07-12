208743352d7b68a235c03bf810058db1b4067727
mdegi <mdegiorgio@gmail.com>
2025-07-12 14:48:56 +0200

    Made comment shorter

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 @RestController
 public class ApplicationController implements CommandLineRunner {
 
-	//I am adding some comments here
+	//Making it shorter
 	private final CustomerRepository repository;
 
 	private static final String ALL_VALUES = "ALL";
