55f5a9e8dff05664154445de7a7d95397a1345d4
mdegi <mdegiorgio@gmail.com>
2025-07-15 08:13:51 +0200

    Update ApplicationController.java

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 @RestController
 public class ApplicationController implements CommandLineRunner {
 
-	//I am changing a comment for Peer review
+	//Yes sure good idea
 	private final CustomerRepository repository;
 
 	private static final String ALL_VALUES = "ALL";
