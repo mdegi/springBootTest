f5e915c35aa63ff7ca7eb836b2f4e2cea183d793
MD-Outlook <martindegi@outlook.com>
2025-08-23 18:49:49 +0200

    Update ApplicationController.java

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class ApplicationController implements CommandLineRunner {
 
 
-	//Testing PR
+	//Testing PR with new setup
 	private final CustomerRepository repository;
 
 	private static final String ALL_VALUES = "ALL";
