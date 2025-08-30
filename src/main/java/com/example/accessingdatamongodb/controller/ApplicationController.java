a62dbd24aa05c4d0ba6b4de3bc06c7ca95fc3c9e
mdegi <mdegiorgio@gmail.com>
2025-08-30 15:24:59 +0200

    Mdegi patch 1 (#44)
    * Update ApplicationController.java
    
    * Update ApplicationController.java
    
    * Update ApplicationController.java

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -14,7 +14,7 @@ import java.util.stream.Collectors;
 public class ApplicationController implements CommandLineRunner {
 
 
-	//Testing PR and Linter - seems ok
+	//Testing PR and Linter
 	private final CustomerRepository repository;
 
 	private static final String ALL_VALUES = "ALL";
