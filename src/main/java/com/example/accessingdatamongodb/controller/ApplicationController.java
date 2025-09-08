ae7205cdc4510507fd457776f7253a53a71c5c75
mdegi <mdegiorgio@gmail.com>
2025-09-08 12:52:03 +0200

    Update checkstyle.xml (#55)
    * Update checkstyle.xml
    * Update ApplicationController.java
    
    ---------
    
    Co-authored-by: MD-Outlook <martindegi@outlook.com>

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -10,9 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import java.util.Vector;
+
 @RestController
 public class ApplicationController implements CommandLineRunner {
 
+	//public final static String inCorrectNamedVar = "testValue";
+	public static final String A_CORRECT_NAMED_VAR = "testValue";
 
 	//Testing PR & CI
 	private final CustomerRepository repository;
@@ -26,6 +30,10 @@ public class ApplicationController implements CommandLineRunner {
 		this.customerService = customerService;
 	}
 
+	private void someWrongMethod() {
+		System.out.println("Incorrect naming convention when starting with an uppercase letter");
+	}
+	
 	//This method has been done to test different options in requestMapping annotation
 	//examples can be as follows:
 	//http://localhost:8080/customerName/msisdn:35679632198?fields=DOB
@@ -60,10 +68,6 @@ public class ApplicationController implements CommandLineRunner {
 		}
 	}
 
-	private void someWrongMethod() {
-		System.out.println("Incorrect naming convention when starting with an uppercase letter");
-	}
-	
 	@Override
 	public void run(String... args) {
 		customerService.setRepository(repository);
