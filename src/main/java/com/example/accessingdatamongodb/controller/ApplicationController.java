a7723f6c8b7f7bd80a1cb2ecb235df3b5fa5019b
mdegi <mdegiorgio@gmail.com>
2025-09-25 17:02:34 +0200

    Update ApplicationController.java (#56)
    * Update ApplicationController.java

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import java.util.Vector;
+//import java.util.Vector;
 
 @RestController
 public class ApplicationController implements CommandLineRunner {
