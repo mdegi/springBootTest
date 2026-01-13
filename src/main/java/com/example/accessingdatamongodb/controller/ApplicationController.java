34a2d824df4f31594644028b42d29f6ca787359c
mdegi <mdegiorgio@gmail.com>
2026-01-13 07:46:19 +0100

    Update ApplicationController.java

--- src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
+++ src/main/java/com/example/accessingdatamongodb/controller/ApplicationController.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 @RestController
 public class ApplicationController implements CommandLineRunner {
 
-	//Adding some comment here
+	//Adding some comment here - more comments
 	//public final static String inCorrectNamedVar = "testValue";
 	public static final String A_CORRECT_NAMED_VAR = "testValue";
 
